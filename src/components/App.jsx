f745f86f21e3e234a232eb2a02c99181195982ed
Jainam Gandhi <jainamgandhi02@gmail.com>
2023-04-24 17:16:20 +0530

    First Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(item) {
+    setItems((prevValue) => {
+      return [...prevValue, item];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems((prevValue) => {
+      return prevValue.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addItem} />
+      {items.map((item, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={item.title}
+            content={item.content}
+            onDelete={deleteItem}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
